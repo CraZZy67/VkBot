5c051525632af6967b84d6861760dbf59054ae19
Sergey <128322450+CraZZy67@users.noreply.github.com>
2024-07-19 01:44:54 +0300

    Сделана механика с проверкой на подписку!

--- main.py
+++ main.py
@@ -8,18 +8,33 @@ import os
 load_dotenv()
 
 
+def get_text(file: str) -> str:
+    with open(file, encoding="utf-8") as f:
+        return "".join(f.readlines())
+
+
 def main():
+    trigger_words_s = [
+                        "начать", "start", "/start", "/начать", "Начать", "Start"
+                        "Я подписался!", "я подписался", "Я подписался", "я подписался!"
+                       ]
 
     vk_session = vk_api.VkApi(token=os.getenv("TOKEN"))
     longpoll = VkBotLongPoll(vk_session, os.getenv("GROUP_ID"))
     vk = vk_session.get_api()
 
     for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message["text"] == "start":
+        if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message["text"] in trigger_words_s:
             id_ = event.obj.message["from_id"]
-            kb = VkKeyboard(one_time=False, inline=True)
-            kb.add_button(color=VkKeyboardColor.POSITIVE, label="Я подписался!")
-            vk.messages.send(user_id=id_, random_id=0, message="Добро пожаловать", keyboard=kb.get_keyboard())
+
+            if vk.groups.isMember(group_id=os.getenv("GROUP_ID"), user_id=id_):
+                vk.messages.send(user_id=id_, random_id=0, message=get_text("texts/follow.txt"))
+            else:
+                kb = VkKeyboard(one_time=False, inline=True)
+                kb.add_button(color=VkKeyboardColor.POSITIVE, label="Я подписался")
+
+                vk.messages.send(user_id=id_, random_id=0, message=get_text("texts/un_follow.txt"),
+                                 keyboard=kb.get_keyboard())
 
 
 if __name__ == '__main__':
