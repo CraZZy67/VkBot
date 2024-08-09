234bab1af037dafa9935553d200fdcfe1bc165a2
CraZZy67 <sergei89066026767@gmail.com>
2024-08-09 18:37:04 +0300

    Исправил ошибку с выключением бота при исключении

--- main.py
+++ main.py
@@ -10,40 +10,40 @@ vk = vk_session.get_api()
 
 
 def main():
-    for event in longpoll.listen():
+    while True:
+        try:
+            for event in longpoll.listen():
 
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            user_id = event.obj.message["from_id"]
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    user_id = event.obj.message["from_id"]
 
-            if event.obj.message["text"] in settings1.WORDS:
-                add_user(user_id)
-                check_follow(vk=vk, group_id=settings1.GROUP_ID, user_id=user_id)
+                    if event.obj.message["text"] in settings1.WORDS:
+                        add_user(user_id)
+                        check_follow(vk=vk, group_id=settings1.GROUP_ID, user_id=user_id)
 
-            if str(user_id) in settings1.ADMINS.split(","):
+                    if str(user_id) in settings1.ADMINS.split(","):
 
-                if event.obj.message["text"] == "/users":
-                    text = f"Пользователей в базе данных: {get_len_db()}"
-                    vk.messages.send(user_id=user_id, random_id=0, message=text, keyboard=kb_db_info())
-                    main_logger.info("Информация о БД отправлена в диалог.")
+                        if event.obj.message["text"] == "/users":
+                            text = f"Пользователей в базе данных: {get_len_db()}"
+                            vk.messages.send(user_id=user_id, random_id=0, message=text, keyboard=kb_db_info())
+                            main_logger.info("Информация о БД отправлена в диалог.")
 
-                if event.obj.message["text"] == "Отчистить базу данных":
-                    clear_db()
-                    vk.messages.send(user_id=user_id, random_id=0, message="База данных отчищена")
+                        if event.obj.message["text"] == "Отчистить базу данных":
+                            clear_db()
+                            vk.messages.send(user_id=user_id, random_id=0, message="База данных отчищена")
 
-                if event.obj.message["text"] == "Разослать сообщение":
-                    distribution_text(vk=vk, user_id=user_id)
+                        if event.obj.message["text"] == "Разослать сообщение":
+                            distribution_text(vk=vk, user_id=user_id)
 
-                if event.obj.message["text"] == "/change":
-                    text = "Выберите какой из текстов вам надо заменить."
-                    vk.messages.send(user_id=user_id, random_id=0, message=text, keyboard=kb_texts())
+                        if event.obj.message["text"] == "/change":
+                            text = "Выберите какой из текстов вам надо заменить."
+                            vk.messages.send(user_id=user_id, random_id=0, message=text, keyboard=kb_texts())
+
+        except Exception as ex:
+            main_logger.error(f"Произошла неожиданная ошибка: {ex}")
+            continue
 
 
-try:
-
-    if __name__ == '__main__':
-        main_logger.info("Бот запущен!")
-        main()
-
-except Exception as ex:
-    main_logger.exception(f"Произошла неожиданная ошибка: {ex}")
+if __name__ == '__main__':
+    main_logger.info("Бот запущен!")
     main()
