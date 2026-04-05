1a4dbdf32d742c7f3b7a02af1ea6849a7e3ccf1e
CraZZy67 <sergei89066026767@gmail.com>
2026-04-05 21:21:44 +0300

    Добавил таблицы БД, модуль для логирования, константы и конфиг модуль

---
+++ bot/src/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped
+
+from .config import Base
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_id: Mapped[int] = mapped_column(Integer(), unique=True)
+    token: Mapped[str] = mapped_column(String(), unique=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_id: Mapped[int] = mapped_column(ForeignKey('groups.group_id', ondelete='CASCADE', onupdate='CASCADE'))
+    user_id: Mapped[int]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+
+class Template(Base):
+    __tablename__ = "templates"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_id: Mapped[int] = mapped_column(ForeignKey('groups.group_id', ondelete='CASCADE', onupdate='CASCADE'))
+    subscribed: Mapped[str]
+    not_subscribed: Mapped[str]
+    distribution: Mapped[str]
--- modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sup_functions import *
-from .fsm_functions import handling_docs
-from .keyboards import kb_db_info, kb_texts, buttons
