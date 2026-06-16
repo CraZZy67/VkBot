337744146469cf26ada41e9ad5d9ac0dbfdc387f
CraZZy67 <sergei89066026767@gmail.com>
2026-06-17 01:44:40 +0300

    1.1.1 Не большие изменения

--- bot/src/constants.py
+++ bot/src/constants.py
@@ -1,23 +1,19 @@
-from dotenv import load_dotenv
-
 import os
 
-
-load_dotenv(override=True)
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
 DB_SYSTEM = 'postgresql'
 DB_DRIVER = 'psycopg2'
 
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_NAME = os.getenv('DB_NAME', 'postgres')
-DB_PORT = os.getenv('DB_PORT', '5432')
+DB_USER = os.getenv('POSTGRES_USER', 'postgres')
+DB_NAME = os.getenv('POSTGRES_DB', 'postgres')
+DB_PORT = os.getenv('POSTGRES_PORT', '5432')
 
 EVENT_WAIT = os.getenv('EVENT_WAIT', 10)
 
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
-DB_HOST_NAME = os.getenv('DB_HOST_NAME', 'db')
+DB_HOST_NAME = os.getenv('POSTGRES_HOST_NAME', 'db')
 
 MAX_SYMBOLS = 4096
